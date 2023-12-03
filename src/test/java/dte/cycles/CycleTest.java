56b6eeef4497513c77805887ae979894773bc2b6
David <darknight4123@gmail.com>
2023-12-04 00:23:53 +0200

    Removed the CycleFactory class
    It had no benefits + the cycle creation is now more readable.

--- src/test/java/dte/cycles/CycleTest.java
+++ src/test/java/dte/cycles/CycleTest.java
@@ -4,14 +4,12 @@ import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-import dte.cycles.factory.CycleFactory;
-
 public class CycleTest 
 {
 	@Test
 	public void testOrderedCycle() 
 	{
-		Cycle<Integer> cycle = CycleFactory.ordered(1, 2, 3, 4);
+		Cycle<Integer> cycle = Cycle.ordered(1, 2, 3, 4);
 		
 		Integer[] elements = cycle.getElements().toArray(new Integer[0]);
 		
